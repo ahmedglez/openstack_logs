d0aa204ce53231bd6527332ae641d494d71efd5e
Ahmedglez <ahmediglez@gmail.com>
2022-07-05 14:05:24 -0400

    testing mongodb #2

--- test/dist/test.dev.js
+++ test/dist/test.dev.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var _require = require('../database/insert_Data'),
+var _require = require('../database/insert_data'),
     insert_json_DB = _require.insert_json_DB;
 
 insert_json_DB('nova');
--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 const {
     insert_json_DB
-} = require('../database/insert_Data')
+} = require('../database/insert_data')
 
 
 insert_json_DB('nova')
